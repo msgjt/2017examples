f47474a599ed88157190a803b896eee9b1021446
szucsa <szucsatti87@gmail.com>
2017-05-30 14:24:41 +0300

    Static Import - simple example

---
+++ src/main/java/de/msg/training/examples/StaticImport.java
@@ -0,0 +1,15 @@
+package de.msg.training.examples;
+
+public class StaticImport {
+
+  /**
+   * Refactor this method to use static imports
+   *
+   * @param ags
+   */
+  public static void main(final String ags[]) {
+    System.out.println(Integer.MAX_VALUE);
+    System.out.println(Long.MAX_VALUE);
+  }
+
+}
